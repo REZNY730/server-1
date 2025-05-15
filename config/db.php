d762c68647e9560f8299d37e8c6f2626bbc6eef3
gr426_reepe <repel.egor@gmail.com>
2025-05-15 11:41:51 +0700

    4

---
+++ config/db.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'thjznxgk_m6',
+    'username' => 'thjznxgk',
+    'password' => 'TVeGbd',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => '',
+];
